fa77c00f9037010d3eac9af3447799c7350cce57
Loïs <lois.guillet@gmail.com>
2018-12-12 15:33:41 +0100

    crossorigin

--- js/fluxReader.js
+++ js/fluxReader.js
@@ -1,14 +1,36 @@
-FEED_URL = "https://cors-anywhere.herokuapp.com/https://ade.u-pec.fr/direct/gwtdirectplanning/rss?data=bd72d825015315feb8cbbaa6aa476052100da3b09ab2cbe65e52054f7c46ced7494b77e816454fc2720360c428aacfcc8d964e9df5a0b3482149ccda9220c7109ef5e799eaa34c26";
-$.ajax({
-    url: FEED_URL,
-    type: 'GET',
-    headers: {"origin":"http://maximum.blog"},
-    dataType: "xml",
-    success: function (data) {
-        console.log(data);
-        /*$(data).find("entry").each(function () {
-            var el = $(this);
-            console.log(el);
-        });*/
+if (!String.prototype.format) {
+    String.prototype.format = function() {
+        a = this;
+        for (k in arguments) {
+          a = a.replace("{" + k + "}", arguments[k])
+        }
+        return a
+      }
     }
-  });
+FEED_URL = "https://ade.u-pec.fr/direct/gwtdirectplanning/rss?data=bd72d825015315feb8cbbaa6aa47605216ab163aac257a3f5be8a9664ca2feae65bb6e14989d5c7cc46535c83d94d2b00c06f73294a028d463398f4ea50c44b7061f7968f1d954af";
+template = '<div class="media">'+
+'<div class="media-body bg-dark text-white mb-4 p-3">'+
+    '<div class="row">'+
+    '<div class="col-lg-1 col-md-2 col-sm-4">'+
+    '    <h5 class="mt-0"><span class="badge badge-danger mr-4 rounded-0">{0}</span></h5>'+
+    '    <div>{1}</div>'+
+    '</div>'+
+    '<div class="col-lg-7 col-md-6 col-sm-4 pl-5"><h5>{2}</h5><div>{3}</div> </div>'+
+    '<h4 class="mt-0 col-lg-4 col-md-4 col-sm-4 text-right my-auto">{4}</h4>'+
+    '</div>'+
+'</div>'+
+'</div>';
+
+$.ajax({
+  type: 'GET',
+  url: "https://api.rss2json.com/v1/api.json?rss_url=" + FEED_URL,
+  dataType: 'jsonp',
+  success: function(data) {
+    console.log(data.feed.description);    
+    //console.log(data);
+    data.items.forEach(element => {
+        console.log(element);
+        document.getElementById("slideshowContainer").innerHTML+=template.format("FISA blabla", "ici un autre ruc","autre chose","le tritre du cours",element.description.find("p")[0]);
+    });
+  }
+});
